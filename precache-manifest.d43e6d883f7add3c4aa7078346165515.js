2405aaab3f4db7cccee00784283e5579eca4d38b
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-09 20:48:09 +0100

    Updates

---
+++ precache-manifest.d43e6d883f7add3c4aa7078346165515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12450e18dd0ffb7e48ed5d09cb7377ca",
+    "url": "/reviews-project/index.html"
+  },
+  {
+    "revision": "2834207a9af9622ed3a9",
+    "url": "/reviews-project/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviews-project/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews-project/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2834207a9af9622ed3a9",
+    "url": "/reviews-project/static/js/main.04286d74.chunk.js"
+  },
+  {
+    "revision": "4f8aab8c185f5d608e32",
+    "url": "/reviews-project/static/js/runtime-main.f7486be3.js"
+  }
+]);
